1e7b5417a4e830512bf97c071d4804b8a50c1f6c
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-08 23:18:35 +0400

    first commit

---
+++ VowelLetter/VowelLetter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VowelLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Convert.ToString(Console.ReadLine());
+            int count = VowelLetter(text);
+            Console.WriteLine(count + "-> eded sait var");
+        }
+        static int VowelLetter(string text)
+        {
+            int count = 0;
+            char[] array = { 'a', 'ı', 'o', 'u', 'e', 'ə', 'i', 'ö', 'ü' };
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (text[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
